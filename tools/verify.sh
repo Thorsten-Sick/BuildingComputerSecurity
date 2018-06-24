130377aa6f0e1589545a04edea6213a7bbb1ba96
Thorsten Sick <thorsten.sick@email.de>
2018-06-24 11:24:09 +0200

    verify todo added

---
+++ tools/verify.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Verifying the book for correctness
+#
+# TODO: Language check
+
+# TODO: Check every chapter can be linked
+
+# TODO: Check external links can be reached1
+
+# TODO: check urls are links
+
+# TODO: Check internal links can be reached
